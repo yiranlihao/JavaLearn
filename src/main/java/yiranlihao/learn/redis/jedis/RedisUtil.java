140fc47ba5d42db55e6bb14952249c92a03c19de
lihao <275@op110.com>
2017-04-12 17:09:39 +0800

    test

--- src/main/java/yiranlihao/learn/redis/jedis/RedisUtil.java
+++ src/main/java/yiranlihao/learn/redis/jedis/RedisUtil.java
@@ -54,7 +54,7 @@ public final class RedisUtil {
      */
     public synchronized static Jedis getJedis() {
         try {
-            if (jedisPool == null) {
+            if (jedisPool != null) {
                 Jedis resource = jedisPool.getResource();
                 return resource;
             } else {
@@ -76,4 +76,48 @@ public final class RedisUtil {
             jedisPool.destroy();//jedisPool.close();
         }
     }
+    
+    /** 
+     * 获取连接池. 
+     * @return 连接池实例 
+     */  
+    public static JedisPool getPool() {  
+    	 try {
+             if (jedisPool != null) {
+                 return jedisPool;
+             } else {
+                 return null;
+             }
+         } catch (Exception e) {
+             e.printStackTrace();
+             return null;
+         }
+    }
+
+	public static void closeJedis(Jedis jedis) {
+		// TODO Auto-generated method stub
+		jedis.close();
+	}
+	
+    /** 
+     *类级的内部类，也就是静态的成员式内部类，该内部类的实例与外部类的实例 
+     *没有绑定关系，而且只有被调用到时才会装载，从而实现了延迟加载。 
+     */  
+    private static class RedisUtilHolder{  
+        /** 
+         * 静态初始化器，由JVM来保证线程安全 
+         */  
+        private static RedisUtil instance = new RedisUtil();  
+    }  
+  
+    /** 
+     *当getInstance方法第一次被调用的时候，它第一次读取 
+     *RedisUtilHolder.instance，导致RedisUtilHolder类得到初始化；而这个类在装载并被初始化的时候，会初始化它的静 
+     *态域，从而创建RedisUtil的实例，由于是静态的域，因此只会在虚拟机装载类的时候初始化一次，并由虚拟机来保证它的线程安全性。 
+     *这个模式的优势在于，getInstance方法并没有被同步，并且只是执行一个域的访问，因此延迟初始化并没有增加任何访问成本。 
+     */  
+    public static RedisUtil getInstance() {  
+        return RedisUtilHolder.instance;  
+    }  
+ 
 }
