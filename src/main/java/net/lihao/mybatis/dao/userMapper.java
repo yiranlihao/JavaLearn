69f021498f07f7e9f7544bc1259126d005ba1886
lihao <275@op110.com>
2017-06-06 11:26:28 +0800

    mybatis

---
+++ src/main/java/net/lihao/mybatis/dao/userMapper.java
@@ -0,0 +1,17 @@
+package net.lihao.mybatis.dao;
+
+import net.lihao.mybatis.entity.user;
+
+public interface userMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(user record);
+
+    int insertSelective(user record);
+
+    user selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(user record);
+
+    int updateByPrimaryKey(user record);
+}
